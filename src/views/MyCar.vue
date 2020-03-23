6133325cd8d25a57957175e68699768d7ac93c5d
Blake <blake.tibbitts@hotmail.com>
2020-03-22 19:56:48 -0600

    car infooo

--- src/views/MyCar.vue
+++ src/views/MyCar.vue
@@ -3,78 +3,68 @@
     <div class="row">
       <div class="col-2"></div>
       <div class="col-8">
-        <h3>Car information</h3>
+        <h1 class="main-title">Car information</h1>
       </div>
       <div class="col-2"></div>
     </div>
     <div class="row">
       <div class="col-2"></div>
       <div class="col-8">
-        <div class="car-info">
+        <div v-bind:class="{editinfo : isEdit, carinfo: !isEdit}">
           <template v-if="isEdit">
-            <label>
-              Make:
-              <input type="text" v-model="myCar.make" />
-            </label>
-            <label>
-              Model:
-              <input type="text" v-model="myCar.model" />
-            </label>
-            <label>
-              Year:
-              <input type="text" v-model="myCar.year" />
-            </label>
-            <label>
-              Miles:
-              <input type="text" v-model="myCar.miles" />
-            </label>
-            <label>
-              Picture URL:
-              <input type="text" v-model="myCar.picture" />
-            </label>
+            <div class="flex-r">
+              <label class="inputs">
+                Make:
+                <input type="text" class="form-control" v-model="myCar.make" />
+              </label>
+              <label class="inputs">
+                Model:
+                <input type="text" class="form-control" v-model="myCar.model" />
+              </label>
+            </div>
+            <div class="flex-r">
+              <label class="inputs">
+                Year:
+                <input type="text" class="form-control" v-model="myCar.year" />
+              </label>
+              <label class="inputs">
+                Miles:
+                <input type="text" class="form-control" v-model="myCar.miles" />
+              </label>
+            </div>
+            <div>
+              <label>
+                Picture URL:
+                <input type="text" class="form-control" v-model="myCar.picture" />
+              </label>
+            </div>
             <button @click="toggleEdit()" class="btn btn-success">Save</button>
           </template>
           <template v-if="!isEdit">
-            <div>
+            <div class="card car-card" style="width: 24rem;">
+              <img class="card-img-top" :src="this.myCar.picture" alt="picutre of your car" />
+              <div class="card-body">
+                <h3 class="card-title">{{this.myCar.make }} {{this.myCar.model}}</h3>
+                <div class="flex-c">
+                  <div class="flex-r space-around">
+                    <p>Year: {{this.myCar.year}}</p>
+                    <p>miles: {{this.myCar.miles}}</p>
+                  </div>
+                  <button @click="toggleEdit()" class="btn btn-warning">Edit</button>
+                </div>
+              </div>
+            </div>
+            <!-- <div class="flex-r">
               <p>{{this.myCar.make}} {{ this.myCar.model}}</p>
               <p>{{this.myCar.year}} {{ this.myCar.miles }}</p>
               <img :src="this.myCar.picture" alt="picture of your car" />
-              <button class="btn btn-toolbar">Edit</button>
-            </div>
+              <button @click="toggleEdit()" class="btn btn-toolbar">Edit</button>
+            </div> -->
           </template>
         </div>
       </div>
       <div class="col-2"></div>
     </div>
-    <div class="row">
-      <h4>Notes</h4>
-  </div>
-  <div class="row">
-          
-      </div>
-      <div class="addNote">
-          <h4>Add Note</h4>
-        <label>
-          Title
-          <input type="text" v-model="currNote.title" />
-        </label>
-        <label>
-            Contents
-            <textarea class="form-control" v-model="currNote.body">
-
-            </textarea>
-        </label>
-        <button @click="saveNote()">Save Note</button>
-      </div>
-      <h4 v-if="notes.length == 0">
-          No Notes found
-      </h4>
-      <template v-for="note in notes">
-          <div class="card" :key="note.body">
-              <p>{{note.title}}</p>
-              <p>{{note.body}}</p>
-          </div>
-      </template>
     </div>
 </template>
 
@@ -88,46 +78,64 @@ export default {
   name: "MyCar",
   data() {
     return {
-      isEdit: true,
-      myCar: {
-        make: null,
-        year : null,
-        model : null,
-        miles: null,
-        picture: null
-      },
-      currNote:{
-          title: null,
-          body: null,
+      isEdit: this.$root.$data.carInfo == null,
+      myCar: this.$root.$data.carInfo,
+      currNote: {
+        title: null,
+        body: null
       }
+    };
+  },
+  methods: {
+    toggleEdit() {
+      this.isEdit = !this.isEdit;
+    },
+    saveInfo() {
+      toggleEdit();
+      this.$root.$data.carInfo = JSON.parse(JSON.stringify(myCar));
     }
   },
-  methods:{
-      toggleEdit() {
-          this.isEdit = !this.isEdit;
-      },
-      saveInfo() {
-          toggleEdit();
-          this.$root.$data.carInfo = myCar;
-      },
-      saveNote() {
-          this.$root.$data.notes.push(JSON.parse(JSON.stringify(this.currNote)));
-          this.currNote.title = '';
-          this.currNote.body = '';
+  computed: {
+    notes() {
+      return this.$root.$data.notes;
     },
-  },
-  computed:{
-      notes(){
-          return this.$root.$data.notes;
-      }
   }
 };
 </script>
 
 <style scoped>
 .addNote {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
+}
+.flex-r {
+  display: flex;
+  flex-direction: row;
+}
+.main-title {
+  margin-top: 50px;
+  margin-bottom: 25px;
+}
+.inputs {
+  margin-right: 5px;
+  margin-left: 5px;
+}
+.flex-c {
+  display: flex;
+  flex-direction: column;
+}
+.space-around {
+  justify-content: space-around;
+}
+.editinfo {
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+}
+.carinfo {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 }
 </style>
 
